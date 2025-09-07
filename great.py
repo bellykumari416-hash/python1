a7da3da269219b527c3a5165460acbcecc57f15a
bellykumari416-hash <bellykumari416@gmail.com>
2025-09-07 17:09:19 +0530

    Add files via upload

--- great.py
+++ great.py
@@ -1,1 +1,15 @@
-print("Hi this is my first code in VS code")
+# Creating Different varibales
+x = "5"
+y = "John"
+print(x)
+print(y)
+
+
+codingal ="codingal"
+print(codingal)
+
+
+#User input
+name = input("enter your name: ")
+
+print("\nHello", "\nwelcome to codignal")
