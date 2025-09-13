9bb5b809aace5785f5e42030861523215e78ee32
bellykumari416-hash <bellykumari416@gmail.com>
2025-09-13 21:09:22 +0530

    congrats.py

---
+++ congrats.py
@@ -0,0 +1,5 @@
+# Get input from the user
+user_input = input("Enter something to celebrate: ")
+
+# Print a congratulatory message including the user's input
+print(f"Congratulations on your {user_input}!")
