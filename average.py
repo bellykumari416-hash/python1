0ed9de0573e00223384e8c7d99a38a2b035c3606
bellykumari416-hash <bellykumari416@gmail.com>
2025-09-14 16:35:10 +0530

    average.py

---
+++ average.py
@@ -0,0 +1,14 @@
+# Storing Values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finging the total of tree
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 tree is: ", sum)
+
+#Finding the average
+average = sum/5
+print("the average of the all the tree is :", average)
