1df6441f58b7398e13e61e122a3c772f8b7a98be
bellykumari416-hash <bellykumari416@gmail.com>
2025-11-09 16:24:52 +0530

    datetime.py

---
+++ datetime.py
@@ -0,0 +1,5 @@
+from datetime import date , time , datetime
+today = date.today()
+now = datetime.now()
+print("Today's date is", today)
+print("n\Date components", today.year, today.month, today.day )
